fb069436c75278d49111b12ad494b038c2f8c056
Josh Bryant <jbryantjersey@hotmail.co.uk>
2021-03-14 20:58:30 +0000

    Added logic

--- Calculator/Calculator/MainForm.cs
+++ Calculator/Calculator/MainForm.cs
@@ -1,75 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calculator
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private string expression;
+        private string newTerm;
+
+        public MainForm()
         {
             InitializeComponent();
         }
 
-        private void SevenButton_Click(object sender, EventArgs e)
+        private void UpdateDisplay(string newExpression, string newTerm)
         {
-
+            ExpressionBox.Text = newExpression;
+            ResultDisplay.Text = newTerm;
         }
 
-        private void EightButton_Click(object sender, EventArgs e)
-        {
+        private void SevenButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "7", newTerm += "7");
 
-        }
+        private void EightButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "8", newTerm += "8");
 
-        private void NineButton_Click(object sender, EventArgs e)
-        {
+        private void NineButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "9", newTerm += "9");
 
-        }
+        private void FourButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "4", newTerm += "4");
 
-        private void FourButton_Click(object sender, EventArgs e)
-        {
+        private void FiveButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "5", newTerm += "5");
 
-        }
+        private void SixButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "6", newTerm += "6");
 
-        private void FiveButton_Click(object sender, EventArgs e)
-        {
+        private void OneButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "1", newTerm += "1");
 
-        }
+        private void TwoButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "2", newTerm += "2");
 
-        private void SixButton_Click(object sender, EventArgs e)
-        {
+        private void ThreeButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "3", newTerm += "3");
 
-        }
+        private void SingleZeroButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "0", newTerm += "0");
 
-        private void OneButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TwoButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void ThreeButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void SingleZeroButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void DoubleZeroButton_Click(object sender, EventArgs e)
-        {
-
-        }
+        private void DoubleZeroButton_Click(object sender, EventArgs e) => UpdateDisplay(expression += "00", newTerm += "00");
     }
 }
